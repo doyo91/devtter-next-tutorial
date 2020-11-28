90790a7788bd8c29e35ad24c5f82936e432dd811
doyo91 <doyo_91@hotmail.com>
2020-11-28 18:57:26 +0100

    Styles

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,54 @@
 import AppLayout from "../components/AppLayout";
-import { LinkCustom } from "../components/LinkCustom"; // para SPA
-import styles from "../styles/Home.module.css";
+import Button from "../components/Button";
+import GitHub from "../components/Icons/GitHub";
+import { colors } from "../styles/theme";
 
 export default function Home() {
   return (
-    <AppLayout>
-      <h1 className={styles.title}>
-        <a href="/">devter</a>
-      </h1>
-      <nav className={styles.nav}>
-        <LinkCustom path="/timeline">
-          <a>timeline</a>
-        </LinkCustom>
-      </nav>
-    </AppLayout>
+    <>
+      <AppLayout>
+        <section>
+          <img src="/devter-logo.png" alt="logo" />
+          <h1>Devtter</h1>
+          <h2>Talk about development with developers 👨‍💻👩‍💻</h2>
+          <div>
+            <Button>
+              <GitHub fill="#fff" width={24} height={24} />
+              Login with GitHub
+            </Button>
+          </div>
+        </section>
+      </AppLayout>
+
+      <style jsx>{`
+        section {
+          display: grid;
+          place-content: center;
+          place-items: center;
+          height: 100%;
+        }
+        img {
+          width: 12rem;
+        }
+
+        h1 {
+          font-size: 2.4rem;
+          color: ${colors.secondary};
+          font-weight: 800;
+          margin-bottom: 1.6rem;
+        }
+
+        h2 {
+          font-size: 2.1rem;
+          margin: 0;
+          color: ${colors.primary};
+          max-width: 25rem;
+        }
+
+        div {
+          margin-top: 1.6rem;
+        }
+      `}</style>
+    </>
   );
 }
