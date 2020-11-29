586510184691e3bfd3862c1186c82132ef3ba3ad
doyo91 <doyo_91@hotmail.com>
2020-11-29 17:33:31 +0100

    login

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,28 @@ import Button from "../components/Button";
 import GitHub from "../components/Icons/GitHub";
 import { colors } from "../styles/theme";
 
+import {
+  loginWithGithub,
+  onAuthStateChanged,
+  signOut,
+} from "../firebase/client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    onAuthStateChanged(setUser);
+  }, []);
+
+  const handleClick = () => {
+    loginWithGithub()
+      .then(setUser)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <AppLayout>
@@ -12,10 +33,18 @@ export default function Home() {
           <h1>Devtter</h1>
           <h2>Talk about development with developers 👨‍💻👩‍💻</h2>
           <div>
-            <Button>
-              <GitHub fill="#fff" width={24} height={24} />
-              Login with GitHub
-            </Button>
+            {user === null && (
+              <Button onClick={handleClick}>
+                <GitHub fill="#fff" width={24} height={24} />
+                Login with GitHub
+              </Button>
+            )}
+            {user && user.avatar && (
+              <div>
+                <img src={user.avatar} alt="User avatar" />
+                <strong>{user.username}</strong>
+              </div>
+            )}
           </div>
         </section>
       </AppLayout>
