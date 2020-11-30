833c0cf93e1153b8b10156a2e67ddfbaf1ad7e8f
doyo91 <doyo_91@hotmail.com>
2020-11-30 11:04:22 +0100

    fix login

--- firebase/client.js
+++ firebase/client.js
@@ -20,7 +20,7 @@ const mapUserFromFirebaseAuthToUser = (user) => {
 
 export const onAuthStateChanged = (onChange) => {
   return firebase.auth().onAuthStateChanged((user) => {
-    const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+    const normalizedUser = user ? mapUserFromFirebaseAuthToUser(user) : null
     onChange(normalizedUser)
   })
 }
