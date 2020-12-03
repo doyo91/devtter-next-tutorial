67405ed6d8594deac0040a35318e1f03375ff427
doyo91 <doyo_91@hotmail.com>
2020-12-03 15:20:08 +0100

    add getServerSideProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -22,14 +22,32 @@ export default function DevitPage(props) {
   )
 }
 
-DevitPage.getInitialProps = (context) => {
-  const { query, res } = context
-  const { id } = query
-  return fetch(`http://localhost:3000/api/devits/${id}`).then((apiResponse) => {
-    if (apiResponse.ok) return apiResponse.json()
-    if (res) {
-      // res.writeHead(404).end()
-      res.writeHead(301, { Location: "/notFound404" }).end()
+export async function getServerSideProps(context) {
+  const { params, res } = context
+  const { id } = params
+
+  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+
+  if (apiResponse.ok) {
+    const props = await apiResponse.json()
+    return {
+      props,
     }
-  })
+  }
+  if (res) {
+    // res.writeHead(404).end()
+    res.writeHead(301, { Location: "/notFound404" }).end()
+  }
 }
+
+// DevitPage.getInitialProps = (context) => {
+//   const { query, res } = context
+//   const { id } = query
+//   return fetch(`http://localhost:3000/api/devits/${id}`).then((apiResponse) => {
+//     if (apiResponse.ok) return apiResponse.json()
+//     if (res) {
+//       // res.writeHead(404).end()
+//       res.writeHead(301, { Location: "/notFound404" }).end()
+//     }
+//   })
+// }
